32f08c3442403753365d0155512599e32aae39ed
someshrathore27 <someshrathore27@gmail.com>
2025-03-12 08:22:00 +0530

    Frontend Setup

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 
-const Footer = () => {
+const Footer = () => { 
   return (
     <div className='md:mx-10'>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
